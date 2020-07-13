c8f647ef0139a9df523ef1911c54f2dc56de0e90
Zondazx <mzxgd16@gmail.com>
2020-07-13 12:55:45 -0400

    typo

--- ajax-type-ahead/app.js
+++ ajax-type-ahead/app.js
@@ -1,7 +1,6 @@
 /*
 TODO: Sort by Geolocation (nearest cities)
 */
-
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 const searchInput = document.querySelector(".search");
