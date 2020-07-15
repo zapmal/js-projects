ac50c72c7df13f5a71c7d84026e082c51fec2776
Zondazx <mzxgd16@gmail.com>
2020-07-15 15:15:16 -0400

    Added custom video player

---
+++ custom-video-player/app.js
@@ -0,0 +1,53 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+let mouseDown = false;
+
+function togglePlay() {
+    video[video.paused ? "play" : "pause"]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    const timeToSkip = parseFloat(this.dataset.skip);
+    video.currentTime += timeToSkip;
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button => button.addEventListener("click", skip));
+
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", e => mouseDown && scrub(e));
+progress.addEventListener("mousedown", () => mouseDown = true);
+progress.addEventListener("mouseup", () => mouseDown = false);
