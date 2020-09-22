86d9abdfa74c0674d9dcfe0f056862f8f2576515
Zondazx <mzxgd16@gmail.com>
2020-09-22 14:38:50 -0400

    finished, forgot to commit this

--- 32 - APIs and Fetch/app.js
+++ 32 - APIs and Fetch/app.js
@@ -3,6 +3,8 @@ const charactersDropdown = document.getElementById("characters");
 const characterImage = document.querySelector(".image img");
 const characterName = document.querySelector(".name");
 const characterNickname = document.querySelector(".nickname");
+const characterPortrayedBy = document.querySelector(".portrayed");
+const characterAppearances = document.querySelector(".appearances");
 const error = document.querySelector(".error");
 
 async function retrieve() {
@@ -34,7 +36,7 @@ async function loadSelected(character) {
 
             displayRetrievedData(data);
         } catch {
-            displayError("Sorry, the image you requested couldn't be displayed, please try again later.");
+            displayError("Sorry, there was an error loading the page, please try again later.");
         }
     }
 }
@@ -43,7 +45,12 @@ function displayRetrievedData(data) {
     data.forEach(d => {
         characterImage.src = d.img;
         characterName.textContent = d.name;
-        characterNickname.textContent = d.nickname;
+        characterNickname.textContent = `Nickname: ${d.nickname}`;
+        characterPortrayedBy.textContent = `Portrayed by: ${d.portrayed}`;
+        characterAppearances.textContent = `Appeared in seasons: ${d.appearance}`;
+
+        const dddd = d.appearance;
+        console.log(dddd);
     });
 }
 
@@ -51,12 +58,14 @@ characterImage.addEventListener("error", () => {
     characterImage.src = "default_image.jpg";
     characterName.textContent = "Image of the Show";
     characterNickname.textContent = "";
+    characterPortrayedBy.textContent = "";
+    characterAppearances.textContent = "";
     displayError("Sorry, the image you requested couldn't be displayed, please try again later.");
 });
 
 function displayError(message) {
     error.textContent = message;
-    setTimeout(() => error.textContent = "", 2000);
+    setTimeout(() => error.textContent = "", 4000);
 }
 
 retrieve();
