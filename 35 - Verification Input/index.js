6a584b8c906975ac95e6650e24efe0a9e59b808b
Zondazx <mzxgd16@gmail.com>
2020-11-04 13:14:28 -0400

    added number verification with extra features

---
+++ 35 - Verification Input/index.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("[name='verify']");
+const inputs = form.querySelectorAll(".inputs input");
+
+function handleInput(e) {
+  const input = e.target;
+
+  if (input.nextElementSibling && input.value) {
+    input.nextElementSibling.focus();
+  }
+}
+
+function handlePaste(e) {
+  const paste = e.clipboardData.getData("text");
+
+  inputs.forEach((input, index) => {
+    input.value = paste[index] || "";
+  });
+
+  const isFilled = [...inputs].every(i => i.value !== "");
+
+  if (isFilled) {
+    inputs.forEach(input => input.classList.add("correct"));
+    setTimeout(() => form.submit(), 1000);
+  }
+}
+
+inputs[0].addEventListener("paste", handlePaste);
+form.addEventListener("input", handleInput);
